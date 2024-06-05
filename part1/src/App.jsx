b7a30f04d8a357adf6c4f2d62ef5061fcd7857cb
Donnie Hayes <donovanjhayes@pm.me>
2024-06-05 10:55:40 -0400

    create part 1

---
+++ part1/src/App.jsx
@@ -0,0 +1,59 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const Header = (props) => {
+    console.log(props)
+    return (
+      <>
+      <h1>{props.course}</h1>
+      </>
+    )
+  }
+
+  const Content = (props) => {
+    console.log(props)
+    return (
+      <>
+      <p>
+      {part1.name} {part1.exercises}
+      </p>
+      <p>
+      {part2.name} {part2.exercises}
+      </p>
+      <p>
+      {part3.name} {part3.exercises}
+      </p>
+      </>
+    )
+  }
+
+  const Total = () => {
+    return (
+      <>
+      <p>Number of exercises {part1.exercises+part2.exercises+part3.exercises} </p>
+      </>
+    )
+  }
+
+  return (
+    <div>
+      < Header course={course}/>
+      < Content />
+      < Total />
+
+    </div>
+  )
+}
+
+export default App
