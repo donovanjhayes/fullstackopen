b64aeb2422abe10d2f1a76a21b27926faf52b232
Donnie Hayes <donovanjhayes@pm.me>
2024-06-11 09:15:06 -0400

    practicefolder

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,61 +1,17 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fun of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+
+  const [ counter, setCounter ] = useState(0)
 
 
-
-  const Header = () => {
-    return (
-      <>
-      <div>
-        <h1>{course}</h1>
-      </div>
-      </>
-    )
-  }
-
-
-  const Content = ()=> {
-    return parts.map(part =>
-      <>
-      <div key = {part.name}>
-      <p>{part.name} {part.exercises}</p>
-      </div>
-      </>
-  );
-}
-
-  const Total = ()=> {
-    return(
-      <>
-    <div> Total number of exercises: {parts[0].exercises+parts[1].exercises+parts[2].exercises}</div>
-    </>
-    )
-  }
-
-
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
 
   return (
-    <>
-    <div>
-      <Header course={course}/>
-      <Content parts= {parts}/>
-      <Total parts= {parts}/>
-    </div>
-    </>
+    <div>{counter}</div>
   )
 }
 
